f604fb7788b5a10d76f21963f96ed90033ea9d15
Tiago Medeiros <tml0289@gmail.com>
2022-09-22 01:16:31 +0000

    Add functions for users

--- user-api/main.go
+++ user-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -14,25 +15,51 @@ type user struct {
 }
 
 var users = []user{
-	{ID: "1", User: "Eddie", Name: "Eddie Munson", Token: ""},
-	{ID: "2", User: "John", Name: "John Spartan", Token: ""},
-	{ID: "3", User: "Sarah", Name: "Sarah Connor", Token: ""},
+	{ID: "666", User: "Eddie", Name: "Eddie Munson", Token: "RWRkaWUtNjY2"},
+	{ID: "117", User: "John", Name: "John Spartan", Token: "Sm9obi0xMTc="},
+	{ID: "101", User: "Sarah", Name: "Sarah Connor", Token: "U2FyYWgtMTAx"},
 }
 
 func getUser(c *gin.Context) {
 	user := c.Param("user")
-	for _, usr := range users {
+	for x, usr := range users {
 		if usr.User == user {
+			fmt.Println(x, usr)
 			c.IndentedJSON(http.StatusOK, usr)
 			return
 		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Not Found"})
+}
 
+func addUser(c *gin.Context) {
+	var newUser user
+
+	if err := c.BindJSON(&newUser); err != nil {
+		return
+	}
+
+	users = append(users, newUser)
+	c.IndentedJSON(http.StatusCreated, newUser)
+}
+
+func deleteUser(c *gin.Context) {
+
+	user := c.Param("user")
+	for x, usr := range users {
+		if usr.User == user {
+			fmt.Println(x, usr)
+			users = append(users[:x], users[x:]...)
+			return
+		}
 	}
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Not Found"})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/:user", getUser)
+	router.GET("/users/:user", getUser)
+	router.POST("/add/", addUser)
+	router.GET("/delete/:user", deleteUser)
 	router.Run("localhost:8080")
 }
